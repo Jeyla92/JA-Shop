b8112c86cedf4fe8aa4a6610da81c0f6c058251c
Amela Abazovic <amela.a@hotmail.se>
2025-04-07 11:19:36 +0200

    added footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
 import { SpotsComponent } from './spots/spots.component';
- 
-
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroComponent, SpotsComponent],
+  imports: [RouterOutlet, HeroComponent, SpotsComponent, FooterComponent],
   template: `
     <app-hero></app-hero>
     <app-spots></app-spots>
+    <app-footer></app-footer>
     <router-outlet />
   `,
-  styles: []
+  styles: [],
 })
 export class AppComponent {
   title = 'angular-ecomm';
