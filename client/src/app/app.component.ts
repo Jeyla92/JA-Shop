5deab6a331f9661e2d700f87b9b9ba58a41df4ca
Amela Abazovic <amela.a@hotmail.se>
2025-04-07 16:16:12 +0200

    added client, server & database

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
 import { SpotsComponent } from './spots/spots.component';
@@ -26,4 +27,10 @@ import { FooterComponent } from './footer/footer.component';
 })
 export class AppComponent {
   title = 'angular-ecomm';
+
+  constructor(private http: HttpClient) {
+    this.http.get('/api/products').subscribe((products) => {
+      console.log(products);
+    });
+  }
 }
