c97c29042d6d34f0f442021eca6fb33297782449
Jeyla <jeylanajafova@yahoo.com>
2025-04-10 20:46:27 +0200

    Hämtar produkter från databas via API, kopplat till Angular

--- client/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductDetailComponent } from './card/product-detail.component'; // 👈 importera
-
-export const routes: Routes = [
-  {
-    path: 'products/:slug',
-    component: ProductDetailComponent
-  }
-];
--- client/src/app/card/product.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Product {
-    id: number;
-    name: string;
-    brand: string;
-    price: number;
-    imageUrl: string;
-    slug: string; // för URL, t.ex. "svart-tshirt"
-  }
-  
